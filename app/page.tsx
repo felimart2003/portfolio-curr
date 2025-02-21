be9d9f43da29b064bec5e9dbac4c224ba6adca85
Felipe Martinez <felimart71@gmail.com>
2025-02-21 02:13:30 -0500

    Add Footer and hide Services

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Hero from "./components/Hero"
-import About from "./components/About"
-import Experience from "./components/Experience"
-import Skills from "./components/Skills"
-import Services from "./components/Services"
-import Education from "./components/Education"
-import Contact from "./components/Contact"
-import FloatingNav from "./components/floating-nav"
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Skills from "./components/Skills";
+import Services from "./components/Services";
+import Education from "./components/Education";
+import Contact from "./components/Contact";
+import FloatingNav from "./components/floating-nav";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -15,10 +16,11 @@ export default function Home() {
       <About />
       <Experience />
       <Skills />
-      <Services />
+      {/* <Services />  NOT NEEDED */}
       <Education />
       <Contact />
+      <Footer />
     </main>
-  )
+  );
 }
 
